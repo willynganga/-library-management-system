513fcd04c1066af63e75b7fefd15b6bac85b2400
willynganga <willy.mwangi@acrolinx.com>
2024-04-05 08:22:39 +0300

    Fix cascade on deletion of books or patrons

--- src/main/java/com/willythedev/librarymanagementsystem/model/Patron.java
+++ src/main/java/com/willythedev/librarymanagementsystem/model/Patron.java
@@ -1,11 +1,14 @@
 package com.willythedev.librarymanagementsystem.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -40,4 +43,7 @@ public class Patron {
 
   @Column(unique = true)
   private String phoneNumber;
+
+  @OneToMany(mappedBy = "patron", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<BorrowingRecord> borrowingRecord;
 }
